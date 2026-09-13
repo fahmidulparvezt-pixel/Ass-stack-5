90415fb07eeb4efd54b0f26584bcc93c96669372
fahmidulparvez-pixel <fahmidulparvezt@gmail.com>
2026-09-13 20:31:15 +0600

    Added a comment to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// Main application component - it manages tech stack state and layout
+
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
